2235afdbd042b9aa02f59d38976aaef9f9220ab9
saikrishnajami <saikrishnajami60@gmail.com>
2019-12-10 11:34:19 +0000

    This script shows how to use /dev/null for redirecting or not displaying output

---
+++ nooutput.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script uses /dev/null to make sure the commands wont give any ouput but they will be executed.
+echo "Welcome to my page.have a great day"
+echo "Thanks for visting again. But i dont display this time" >>/dev/null
+
