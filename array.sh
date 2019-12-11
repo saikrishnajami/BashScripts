90b77e430ef683f8c03be15825ce2413b333db1d
saikrishnajami <saikrishnajami60@gmail.com>
2019-12-11 08:37:28 +0000

    This script tells how to use arrays and displaying results

--- array.sh
+++ array.sh
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-i#!/bin/bash
-# This script interates thorugh array and displays its values at run time.
-SERVERLIST=("web1" "web2" "web3")
-echo "The server no 2 is:${SERVERLIST[2]}"
-COUNT=0
-for INDEX in ${SERVERLIST[@]}; do
-  echo "The servers in the list are: ${SERVERLIST[COUNT]}"
-  COUNT="`expr $COUNT + 1`"
-done
-=======
 #!/bin/bash
 # This is example of an array.
 SERVERS=("webs1" "webs2" "webs3")
@@ -18,8 +7,3 @@ for INDEX in ${SERVERS[@]}; do
   echo "Processing server: ${SERVERS[COUNT]}"
   COUNT="`expr $COUNT + 1`"
 done
-
-
-
-
->>>>>>> 2b53c15f7ca9c5d0d151c6d420191fb7432d5e6d
