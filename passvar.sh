8e847a80b5ca32db3a005723c5011499e11d21fe
saikrishnajami <saikrishnajami60@gmail.com>
2019-12-12 12:48:46 +0000

    This script tells how to decalre values to varibles at run time

---
+++ passvar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script tells how to use variables to pass values in script at run time
+echo "Welcome to script $1 $2"
+USERNAME=$1
+PASSWORD=$2
+echo "Myusername is $USERNAME and MyPassword is $PASSWORD"
+
